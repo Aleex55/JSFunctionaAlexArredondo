98eb0bcb69ee9846d9741ea5f900a5438717cb99
Aleex55 <alex.arredondo@institutvidreres.cat>
2021-11-26 12:57:31 +0100

    Ex 1

---
+++ Ex1.js
@@ -0,0 +1,8 @@
+var nom;
+var lastName1;
+var lastName2;
+
+function myName(nom, lastName1, lastName2){
+    console.log(nom + " " + lastName1 + " " + lastName2)
+}
+myName("Alex", "Arredondo", "Rodriguez");
